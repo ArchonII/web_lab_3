b244f0f230d646deb46b686a0ee05fe454b09b83
ArchonII <uriypotrehaev@yandex.ru>
2022-01-07 11:13:28 +0300

    clear code

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -12,31 +12,6 @@ class AnswerController extends Controller
 
     public function list($id)
     {
-        $arr[0][0][0]="author 1";
-        $arr[0][0][1]="text answer on quest 1";
-        $arr[0][1][0]="author 2";
-        $arr[0][1][1]="text 2 answer on quest 1 abc";
-        $arr[0][2][0]="author 3";
-        $arr[0][2][1]="text 3 answer on quest 1 def";
-        $arr[1][0][0]="author 1";
-        $arr[1][0][1]="text answer on quest 2";
-        $arr[1][1][0]="author 2";
-        $arr[1][1][1]="text 2 answer on quest 2 abc";
-        $arr[1][2][0]="author 3";
-        $arr[1][2][1]="text 3 answer on quest 2 def";
-        $arr[2][0][0]="author 1";
-        $arr[2][0][1]="text answer on quest 3";
-        $arr[2][1][0]="author 2";
-        $arr[2][1][1]="text 2 answer on quest 3 abc";
-        $arr[2][2][0]="author 3";
-        $arr[2][2][1]="text 3 answer on quest 3 def";
-/*
-        if ($id > 2) {
-            throw new NotFoundHttpException('вопрос не найден');
-        }
-
-        return $arr[$id];   */
-
         $answer =  Answer::query()
                     ->where(['id'=> $id])
                     ->get();
